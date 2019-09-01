76921aa55f11d39e593a84260d0f2c14de729a30
GadgetLaboratory <crazygadget466@gmail.com>
2019-08-31 22:35:24 -0400

    Updated license, did questions, readme changed.

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print("""My name is Larry Hollins Jr; I struggle with caffeine maintenance and I'm hopelessly addicted to games such that I want to make them my life.  And so, here I am.
+I have a lot of games that I love deeply, but not that I would really consider my favorite; I've never thought about it.  If I had to answer decisively here and now, I would say "Celeste" by Matt Makes Games.
+Honestly, I'm concerned abouting able to stay on pace in this class, at least at the time of writing.  Not because I'm not invested, but solely because I don't even have my own computer so it's difficult to start and end work as I feel comfortable...
+I'm excited about being able to just learn to **make games.**  This is the core skill with which I'm going to make my dreams come true, why wouldn't I be excited?
+User no. for Stack Overflow is 12004613, and the url to this GitHub is https://github.com/GadgetLaboratory.""")
